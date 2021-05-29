46dc0b65c7bf44e49170f4aa17ff32edd49adb41
Antony <vontonnie@gmail.com>
2021-05-29 16:16:48 +0300

    Convert to Databinding Layout

---
+++ app/src/main/java/com/androidshowtime/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.androidshowtime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.androidshowtime.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+ private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
+    }
+}
