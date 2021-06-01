364bf135e8465bcb1bc6eba7ba3c89caf3dfcd6d
Antony <vontonnie@gmail.com>
2021-06-01 20:08:24 +0300

    Make binding observe LiveData

--- app/src/main/java/com/androidshowtime/MainActivity.kt
+++ app/src/main/java/com/androidshowtime/MainActivity.kt
@@ -2,17 +2,26 @@ package com.androidshowtime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.androidshowtime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
  
  private lateinit var binding: ActivityMainBinding
+
+ val viewModel:MyViewModelClass by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
 
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        //link viewModels together
+        binding.viewModel = viewModel
+
+        //make binding observe LiveData
+        binding.lifecycleOwner = this
 
 
     }
