ef0c6e6a29ef944c80c99c2446f6a1b54a6d6dc7
Antony <vontonnie@gmail.com>
2021-06-01 20:04:59 +0300

    Add Activity API

--- app/src/main/java/com/androidshowtime/MainActivity.kt
+++ app/src/main/java/com/androidshowtime/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.databinding.DataBindingUtil
 import com.androidshowtime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
- val viewModel:MyViewModelClass by activityViewModels()
+ 
  private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
