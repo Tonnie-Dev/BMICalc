a48f8070741f24f950a73a3d6ad00272ab9875bc
Antony <vontonnie@gmail.com>
2021-06-02 20:16:56 +0300

    Write getWeight() method

--- app/src/main/java/com/androidshowtime/MyViewModelClass.kt
+++ app/src/main/java/com/androidshowtime/MyViewModelClass.kt
@@ -7,25 +7,51 @@ import androidx.lifecycle.ViewModel
 class MyViewModelClass : ViewModel() {
 
     //Sliders
-    val weightSliderValue = MutableLiveData<Float>()
-    val heightSliderValue = MutableLiveData<Float>()
-    val inchSliderValue = MutableLiveData<Float>()
+    val weightSliderValue = MutableLiveData(0.0F)
+    val heightSliderValue = MutableLiveData(0.0F)
+    val inchSliderValue = MutableLiveData(0.0F)
 
     //Switches
     val isPoundsChecked = MutableLiveData<Boolean>()
     val isFeetChecked = MutableLiveData<Boolean>()
 
 
-fun getHeight(): Double {
+    fun getHeight(): Double {
 
+        return when (isFeetChecked.value) {
 
-    return 0.0
-}
+            true -> {
+
+                (heightSliderValue.value?.times(0.3048))!! + (inchSliderValue.value?.times(0.0254))!!
+            }
+            else -> {
+
+                (heightSliderValue.value?.div(100))?
+
+                !!
+            }
+        }
+    }
 
 
     fun getWeight(): Double {
 
+      return when(isPoundsChecked.value){
 
-        return 0.0
+
+          true -> {
+
+              weightSliderValue.value?.div(2.20462)!!
+          }
+          else -> {
+
+              weightSliderValue.value?.toDouble()!!
+          }
+      }
+
+
+
     }
+
+
 }
